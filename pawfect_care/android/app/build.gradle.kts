ca1cb02bed62385a9a1db6f72d5551f25a2872e0
donatedsalt <mmohsinkhan3685@gmail.com>
2025-09-12 01:05:16 +0500

    fix: trying to fix apk build

--- pawfect_care/android/app/build.gradle.kts
+++ pawfect_care/android/app/build.gradle.kts
@@ -1,3 +1,5 @@
+import java.util.Properties
+
 plugins {
     id("com.android.application")
     // START: FlutterFire Configuration
@@ -7,6 +9,17 @@ plugins {
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
 }
+
+dependencies {
+    // Import the Firebase BoM
+    implementation(platform("com.google.firebase:firebase-bom:34.2.0"))
+    // TODO: Add the dependencies for Firebase products you want to use
+    // When using the BoM, don't specify versions in Firebase dependencies
+    implementation("com.google.firebase:firebase-analytics")
+    // Add the dependencies for any other desired Firebase products
+    // https://firebase.google.com/docs/android/setup#available-libraries
+}
+
 
 android {
     namespace = "com.example.pawfect_care"
@@ -33,11 +46,36 @@ android {
         versionName = flutter.versionName
     }
 
+    // Load keystore properties using Kotlin syntax
+    val keystoreProperties = Properties()
+    val keystorePropertiesFile = rootProject.file("key.properties")
+    if (keystorePropertiesFile.exists()) {
+        keystorePropertiesFile.inputStream().use { inputStream ->
+            keystoreProperties.load(inputStream)
+        }
+    }
+
+    signingConfigs {
+        create("release") {
+            if (project.properties.containsKey("MYAPP_RELEASE_STORE_FILE")) {
+                storeFile = file(project.properties["MYAPP_RELEASE_STORE_FILE"] as String)
+                storePassword = project.properties["MYAPP_RELEASE_STORE_PASSWORD"] as String
+                keyAlias = project.properties["MYAPP_RELEASE_KEY_ALIAS"] as String
+                keyPassword = project.properties["MYAPP_RELEASE_KEY_PASSWORD"] as String
+            } else {
+                storeFile = file(keystoreProperties.getProperty("storeFile"))
+                storePassword = keystoreProperties.getProperty("storePassword")
+                keyAlias = keystoreProperties.getProperty("keyAlias")
+                keyPassword = keystoreProperties.getProperty("keyPassword")
+            }
+        }
+    }
+
     buildTypes {
-        release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
-            signingConfig = signingConfigs.getByName("debug")
+        getByName("release") {
+            // This is the important change. We are now using the new release signing config.
+            signingConfig = signingConfigs.getByName("release")
+            // You can add your own release-specific settings here, like code shrinking.
         }
     }
 }
