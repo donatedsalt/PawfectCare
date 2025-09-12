4eff502e2689cafd15673c170424bbb62c13dc5a
Huzaifa0337 <huzaifatariqaptech@gmail.com>
2025-09-12 11:11:05 +0500

    Profile Pages & Store Image Upload To Firebase Storage

--- pawfect_care/lib/pages/store/more_page.dart
+++ pawfect_care/lib/pages/store/more_page.dart
@@ -1,55 +1,59 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
-class MorePageAppBar extends StatefulWidget implements PreferredSizeWidget {
+import 'package:pawfect_care/widgets/account_profile.dart';
+
+import 'package:pawfect_care/pages/store/profile_page.dart';
+
+class MorePageAppBar extends StatelessWidget implements PreferredSizeWidget {
   const MorePageAppBar({super.key});
 
-  @override
-  State<MorePageAppBar> createState() => _MorePageAppBarState();
-
-  @override
-  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
-}
-
-class _MorePageAppBarState extends State<MorePageAppBar> {
   @override
   Widget build(BuildContext context) {
     return AppBar(title: const Text('More'));
   }
+
+  @override
+  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
 }
 
 class MorePage extends StatelessWidget {
   const MorePage({super.key});
   @override
   Widget build(BuildContext context) {
-    // Get the current user from Firebase Auth
     final user = FirebaseAuth.instance.currentUser;
 
     return ListView(
       padding: const EdgeInsets.all(16),
       children: [
-        // welcome user and more icon
+        // welcome user
         Padding(
           padding: const EdgeInsets.symmetric(vertical: 64),
-          child: Text(
-            "Welcome, ${user?.displayName ?? 'User'}!",
-            textAlign: TextAlign.center,
-            style: const TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+          child: Column(
+            children: [
+              AccountProfile(user: user?.displayName, imageURL: user?.photoURL),
+              SizedBox(height: 16),
+              Text(
+                "Welcome, ${user?.displayName ?? 'User'}!",
+                textAlign: TextAlign.center,
+                style: const TextStyle(fontSize: 24),
+              ),
+            ],
           ),
         ),
 
         // Settings header and list of settings
         Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            const Text(
-              "Settings",
-              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-            ),
             ListTile(
               leading: const Icon(Icons.person),
               title: const Text("Profile"),
-              onTap: () {},
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const ProfilePage()),
+                );
+              },
             ),
             ListTile(
               leading: const Icon(Icons.notifications),
